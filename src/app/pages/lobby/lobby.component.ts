8fb7fdb0832c0bd2793890ef67aba57737cbb5be
JoschuaGoetz <joschua.goetz@gmail.com>
2021-03-08 02:47:37 +0100

    Add a button to copy the code

--- src/app/pages/lobby/lobby.component.ts
+++ src/app/pages/lobby/lobby.component.ts
@@ -2,7 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {AdminClientConnectionService} from '../../../common/services/admin-client-connection.service';
 import {LogService} from '../../../common/services/log.service';
 import {AdminClientService} from '../../../common/services/admin-client.service';
-
+import {Clipboard} from '@angular/cdk/clipboard';
 
 @Component({
     selector: 'app-lobby',
@@ -12,10 +12,14 @@ import {AdminClientService} from '../../../common/services/admin-client.service'
 export class LobbyComponent implements OnInit {
 
     constructor(private logger: LogService, public adminClientConnectionService: AdminClientConnectionService,
-                private adminClientService: AdminClientService) {
+                private adminClientService: AdminClientService, private clipboard: Clipboard) {
     }
 
     ngOnInit(): void {
+    }
+
+    public saveToClipboard(): void {
+        this.clipboard.copy(this.adminClientConnectionService.code);
     }
 
     public startGame(): void {
